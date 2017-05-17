f51e3b64558b02feffc20839bfb2115d4f2dedea
melodytempleton <mtempletoncantu@gmail.com>
2017-05-17 11:14:14 -0500

    Circle bonuses complete

--- src/CircleApp.java
+++ src/CircleApp.java
@@ -3,11 +3,29 @@
  */
 public class CircleApp {
 
+    private static int numberOfCircles = 0;
+
+    public static void makingCircles (){
+        boolean keepMakingCircles = true;
+
+        while (keepMakingCircles) {
+            Input usersInput = new Input();
+            double radius = usersInput.getdouble("Enter the radius of your circle");
+            Circle userCircle = new Circle(radius);
+            System.out.println("The area is " + userCircle.getArea());
+            System.out.println("The circumference is " + userCircle.getCircumference());
+            numberOfCircles++;
+            keepMakingCircles = usersInput.yesNo("Do you want to make another circle?");
+        }
+
+        System.out.println("You have made "+numberOfCircles+" circles.");
+
+    }
+
+
     public static void main(String[] args) {
-        Input usersInput = new Input();
-        double radius = usersInput.getdouble("Enter the radius of your circle");
-        Circle userCircle = new Circle(radius);
-        System.out.println("The area is " + userCircle.getArea());
-        System.out.println("The circumference is " + userCircle.getCircumference());
+
+        makingCircles();
+
     }
 }
