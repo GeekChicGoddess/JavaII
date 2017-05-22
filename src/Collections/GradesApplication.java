cb3ba89e60245d4646701def9cb1aa400bf159bd
melodytempleton <mtempletoncantu@gmail.com>
2017-05-22 15:42:11 -0500

    Complete exercise 3

--- src/Collections/GradesApplication.java
+++ src/Collections/GradesApplication.java
@@ -2,35 +2,89 @@ package Collections;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by melodytempleton on 5/22/17.
  */
 public class GradesApplication {
 
+     public static Scanner scanner = new Scanner(System.in);
+
+    public static  void getStudentInfo(Map <String, Student> list){
+
+
+        System.out.println("Here are the Github usernames of our students");
+       printOutUsernames(list);
+
+    }
+    public static void printOutUsernames(Map <String, Student> list){
+        list.forEach((k, v) -> {
+            System.out.println(k);
+
+        });
+        System.out.println("What student would you like to see more information on?");
+        String input = scanner.nextLine();
+        if (list.get(input) == null){
+            System.out.println("Sorry, no student found with the gihub username of \""+ input + "\".");
+
+        }
+
+        else {
+            System.out.println("Name :" + list.get(input).getName() + "  - Github Username:  " + input);
+            System.out.println("Current Average:  " + list.get(input).getGradeAverage());
+
+        }
+        seeAnotherStudent(list);
+    }
+
+
+    public static void seeAnotherStudent(Map <String, Student> list){
+        System.out.println("Would you like to see another student?");
+        String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")){
+            printOutUsernames(list);
+        }
+        else {
+            System.out.println("Goodbye, and have a wonderful day!");
+            System.exit(0);
+        }
+    }
+
+
+
+
     public static void main(String[] args) {
-        Map<String, Student> student = new HashMap<>();
+        Map<String, Student> students = new HashMap<>();
 
         Student joe = new Student("Joe");
+        Student jose = new Student("Jose");
         Student jane = new Student("Jane");
-        student.put("github.com/JoLo", joe);
-        student.put("github.com/AFineJose", (new Student("Jose")));
-        student.put("github.com/Janiac", jane);
-        student.put("github.com/Joolean", (new Student("Julian")));
+        Student julian = new Student("Julian");
+        students.put("JoLo", joe);
+        students.put("AFineJose", jose);
+        students.put("Janiac", jane);
+        students.put("Joolean", julian);
 
         joe.addGrade(45);
         joe.addGrade(75);
         joe.addGrade(60);
-        student.get("github.com/AFineJose").addGrade(67);
-        student.get("github.com/AFineJose").addGrade(98);
-        student.get("github.com/AFineJose").addGrade(87);
+        jose.addGrade(67);
+        jose.addGrade(98);
+        jose.addGrade(87);
         jane.addGrade(90);
         jane.addGrade(70);
         jane.addGrade(80);
-        student.get("github.com/Joolean").addGrade(90);
-        student.get("github.com/Joolean").addGrade(95);
-        student.get("github.com/Joolean").addGrade(97);
+        julian.addGrade(90);
+        julian.addGrade(95);
+        julian.addGrade(97);
+
+        System.out.println("Welcome");
+        getStudentInfo(students);
+
 
     }
+
+
 
 }
