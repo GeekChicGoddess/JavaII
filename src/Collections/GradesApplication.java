849b3ad7d4c267b4cbd16a6e5e49fa1cbb542805
melodytempleton <mtempletoncantu@gmail.com>
2017-05-22 14:38:45 -0500

    Complete exercise 2
    added Student Hashmap
    added Student objects and keys to map
    added grade to student objects

--- src/Collections/GradesApplication.java
+++ src/Collections/GradesApplication.java
@@ -9,8 +9,27 @@ import java.util.Map;
 public class GradesApplication {
 
     public static void main(String[] args) {
-        Map student = new HashMap();
+        Map<String, Student> student = new HashMap<>();
 
+        Student joe = new Student("Joe");
+        Student jane = new Student("Jane");
+        student.put("github.com/JoLo", joe);
+        student.put("github.com/AFineJose", (new Student("Jose")));
+        student.put("github.com/Janiac", jane);
+        student.put("github.com/Joolean", (new Student("Julian")));
+
+        joe.addGrade(45);
+        joe.addGrade(75);
+        joe.addGrade(60);
+        student.get("github.com/AFineJose").addGrade(67);
+        student.get("github.com/AFineJose").addGrade(98);
+        student.get("github.com/AFineJose").addGrade(87);
+        jane.addGrade(90);
+        jane.addGrade(70);
+        jane.addGrade(80);
+        student.get("github.com/Joolean").addGrade(90);
+        student.get("github.com/Joolean").addGrade(95);
+        student.get("github.com/Joolean").addGrade(97);
 
     }
 
